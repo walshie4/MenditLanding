5c6a5a68a41dd956d7f0e72b5e80d3a4adbf8150
Adam Walsh <adamwalsh14@gmail.com>
2015-03-24 13:04:13 -0400

    Updated baseline of nav

--- config.rb
+++ config.rb
@@ -10,6 +10,7 @@
 ###
 # Page options, layouts, aliases and proxies
 ###
+set :haml, { :ugly => false, :format => :html5 }
 
 # Per-page layout changes:
 #
@@ -23,22 +24,16 @@
 # with_layout :admin do
 #   page "/admin/*"
 # end
+page "index.html", :layout => :base
 
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+configure :development do
+  activate :livereload
+  activate :directory_indexes
+end
 
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+activate :deploy do |deploy|
+      deploy.method = :git
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -56,17 +51,16 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
-
+  activate :minify_javascript
+  activate :gzip
+  activate :minify_html
+  activate :directory_indexes
   # Enable cache buster
-  # activate :asset_hash
+  #activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  #activate :relative_assets
 end
